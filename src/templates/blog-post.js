9e4c9c44a8056af6f493ea82cd9fd9ff387f48cd
Dinh-Phong Nguyen <phong@byond.bio>
2021-04-04 21:33:24 +0200

    fixing blog post bottom nav bar

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -43,30 +43,15 @@ const BlogPostTemplate = ({ data, location }) => {
         <Comments />
       </article>
       <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ⇠ {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} ⇢
-              </Link>
-            )}
-          </li>
-        </ul>
+        {previous && (
+          <Link to={previous.fields.slug} rel="prev">⇠ previous post</Link>
+        )}
+        <Link to="/" rel="home">
+          take me home
+        </Link>
+        {next && (
+          <Link to={next.fields.slug} rel="next">next post ⇢</Link>
+        )}
       </nav>
     </Layout>
   )
