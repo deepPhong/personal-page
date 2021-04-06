916804c724a76eb30171868800bf62ba8ed05d68
Dinh-Phong Nguyen <phong@byond.bio>
2021-04-06 09:50:49 +0200

    making the marginnotes labels inline

--- src/components/tufte-marginnote.js
+++ src/components/tufte-marginnote.js
@@ -4,7 +4,7 @@ const Marginnote = (props) => {
   const Tag = props.tag? props.tag: "p";
   return(
     <Tag>
-      <label htmlFor={props.id} className="md:hidden">&#8853;</label>
+      <label htmlFor={props.id} className="md:hidden inline">&#8853;</label>
       <input type="checkbox" id={props.id} className="hidden"/> 
       <span className="hidden checked-sibling:block md:clear-right md:float-right md:mr-tufte-marginnote text-tufte-margin md:w-1/2 md:mt-2 md:mb-0 md:align-baseline md:relative md:block">{props.children}</span>
     </Tag>
