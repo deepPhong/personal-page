771198104c7aa1f46ada35bcfe050f0e4c688b11
Dinh-Phong Nguyen <phong@byond.bio>
2021-04-13 20:03:46 +0200

    fixing image rounded corners (for real for real)

--- src/components/mdx-styles.js
+++ src/components/mdx-styles.js
@@ -8,6 +8,7 @@ const MDXStyles = () => {
     "table-wrapper": "table-wrapper block overflow-x-auto w-full my-4",
     "grvsc-container": "grvsc-container my-4 rounded-md pre-header-sibling:mt-0 pre-header-sibling:rounded-tl-none pre-header-sibling:rounded-tr-none",
     "gatsby-code-title gatsby-remark-code-title": "gatsby-code-title gatsby-remark-code-title py-1.5 px-4 w-full font-mono text-xs rounded-tl-md rounded-tr-md relative inline-block bg-current-pre-header text-current-bg",
+    // "gatsby-image-wrapper": "gatsby-image-wrapper rounded-md"
     // "gatsby-resp-image-wrapper": "gatsby-resp-image-wrapper rounded-md max-w-full mb-6",
     // "gatsby-resp-image-link": "gatsby-resp-image-link rounded-md max-w-full",
     // "gatsby-resp-image-image": "gatsby-resp-image-image rounded-md max-w-full",
@@ -31,8 +32,13 @@ const MDXStyles = () => {
   const replaceClassName = (classNamesReplacements) => {
     return (child) => {
       if (classNamesReplacements.hasOwnProperty(child.props.className)) {
+        console.log(child)
         return React.cloneElement(child, {
           className: classNamesReplacements[child.props.className]
+        })
+      } else if (child.props.mdxType === "GatsbyImage") {
+        return React.cloneElement(child, {
+          className: "rounded-md"
         })
       } else {
         return child
